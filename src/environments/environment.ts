b644825b5643e320d178d0790deaf6c6f041f7c2
Jerald011003 <coninegaming@gmail.com>
2024-11-06 21:35:39 +0800

    add: sent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseKey: '',
-  geminiKey: '',
-  googleClient: ''
+  supabaseUrl: 'https://vduerriglvtnzgqvxrxy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkdWVycmlnbHZ0bnpncXZ4cnh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5MjgxOTIsImV4cCI6MjA0NTUwNDE5Mn0.QeZlQKgtt4RnoMsrxntkopL_FPIavWvai7zFxuCg-Xc',
+  geminiKey: 'AIzaSyDUyvGkuuUfYoByLevHcpynFZZh718_txs',
+  googleClient: '791057404282-4rhs3et9o0mefbmuql6odav8o7oenr2n.apps.googleusercontent.com'
 };
